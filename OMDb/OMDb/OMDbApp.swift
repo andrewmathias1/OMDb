e46c6fcae08349ea0984388c21708296ff64bde1
Andrew Mathias <andrewmathias90@gmail.com>
2022-12-12 16:17:47 +0000

    Project setup

---
+++ OMDb/OMDb/OMDbApp.swift
@@ -0,0 +1,17 @@
+//
+//  OMDbApp.swift
+//  OMDb
+//
+//  Created by Andrew Mathias on 12/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct OMDbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
